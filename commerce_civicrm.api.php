903bdd621f324051a806b1fbd3cf1bb0815a53a1
Stella Power <stella@annertech.com>
2012-06-17 13:42:22 +0100

    fixed coding style and made use of entity wrappers

--- commerce_civicrm.api.php
+++ commerce_civicrm.api.php
@@ -2,7 +2,7 @@
 
 
 /*
- *   $civicrm_address = array(
+ * $civicrm_address = array(
     1 => array(
       'location_type_id'       => 1,
       'is_primary'             => TRUE,
@@ -17,29 +17,36 @@
  */
 
 /**
- * Implementation of hook_commerce_civicrm_params
- * @param $civicrm_address
+ * Implements hook_commerce_civicrm_params().
+ *
+ * @param $params
+ *   civicrm_location_update params array.
  * @param $order
+ *   Commerce order object.
  * @param $cid
+ *   CiviCRM contact id.
  */
 function hook_commerce_civicrm_params(&$params, $order, $cid) {
-  // You can grab the profile of the customer like so..
-  $profile_id = $order->commerce_customer_billing['und'][0]['profile_id'];
-  if (!$profile_id) return;
-  $profile = commerce_customer_profile_load($profile_id);
+  // You can grab the profile of the customer like so.
+  $order_wrapper = entity_metadata_wrapper('commerce_order', $order);
+  $profile = $order_wrapper->commerce_customer_billing->value();
+  $profile_wrapper = entity_metadata_wrapper('commerce_customer_profile', $profile);
+
   // Then alter $params as required to add any custom commerce fields to send to CiviCRM.
-  $params['job_title'] = $profile->field_job_title['und'][0]['value'];
-  $params['current_employer'] = $profile->field_organisation['und'][0]['value'];
+  $params['job_title'] = $profile_wrapper->field_job_title->value();
+  $params['current_employer'] = $profile_wrapper->field_organisation->value();
   $params['phone'] = array(
     array(
       'is_primary' => TRUE,
-      'phone' => $profile->field_phone['und'][0]['value'],
+      'phone' => $profile_wrapper->field_phone->value(),
       'phone_type_id' => 1,
       'location_type' => 'Home'
     )
   );
-  // Update the contact, need this for details like employer, job title etc
+
+  // Update the contact, need this for details like employer, job title etc.
   $params['id'] = $params['contact_id'];
   $contact = civicrm_api('contact', 'update', $params);
-  // It would be good to make these mappable through a GUI
+  // It would be good to make these mappable through a GUI.
 }
+
